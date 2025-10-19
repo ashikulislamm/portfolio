883afc9e2a2f52095fa6995907f057c5f71ee47f
Ashikul Islam <ashikulislam780@gmail.com>
2025-10-19 19:20:59 +0600

    Remove 'Skills' from navigation items

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -38,7 +38,6 @@ export const Header = () => {
   const navItems = [
     { name: "Home", href: "/" },
     { name: "Projects", href: "/projects" },
-    { name: "Skills", href: "/#skills" },
     { name: "About", href: "/about" },
     { name: "Contact", href: "/contact" },
   ];
